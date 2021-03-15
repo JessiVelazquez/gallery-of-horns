0adf1e8c24649776f95f21854b214cbcfcc5c97d
Jessi <jessivelazq1@gmail.com>
2021-03-15 16:28:01 -0700

    got header, main, and footer displayed on live app

---
+++ src/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div class="header">
+        <header>
+          <h1>This is a header</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+
+export default Header;
